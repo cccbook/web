1bd185b09a5b95b91de87de613ed4ae7699d8ca3
ccckmit <ccckmit@gmail.com>
2019-05-10 10:24:07 +0800

    add oo example

--- code/04-nodejs/02-basic/sum.js
+++ code/04-nodejs/02-basic/sum.js
@@ -1,5 +1,5 @@
 var sum=0;
-for (i=1;i<=10;i++) {
+for (var i=1;i<=10;i++) {
   sum = sum + i;
   console.log("i=", i, " sum=", sum);
 }
---
+++ code/05-javascript/object/person.js
@@ -0,0 +1,14 @@
+var a = [1,2,3]
+
+var p1 = {
+  name: 'ccc',
+  age: 49,
+}
+
+var p2 = {
+  name: 'snoopy',
+  age: 100,
+}
+
+console.log('p1=', p1)
+console.log('p2=', p2)
